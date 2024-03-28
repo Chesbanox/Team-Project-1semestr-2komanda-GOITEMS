998155bcc10f2f499db0ca365f7d97c6e5c1e9d5
Chesbanox <138614833+Chesbanox@users.noreply.github.com>
2024-03-28 14:33:33 +0100

    Update import logging.py

--- import logging.py
+++ import logging.py
@@ -4,7 +4,7 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.types import ParseMode
 from aiogram.utils import executor
 
-API_TOKEN = 'TOKEN'
+API_TOKEN = 'токен'
 
 movies = {
     "Fight club": "https://docs.google.com/document/d/13VL92vH0LI8nDqv4oDpw3NWX8g6RSrE_y1V4zfRJV1w/edit",
@@ -19,24 +19,16 @@ movies = {
     "Diuna":"https://docs.google.com/document/d/1usPjLSNzEiWhOanJpDtDQsyRLi_RH6WIO-eHYkF897w/edit"
 }
 
-# Ініціалізуємо логування
 logging.basicConfig(level=logging.INFO)
 
-# Створюємо об'єкт бота та диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
-# Створюємо словник для зберігання списку фільмів, які користувачі хочуть подивитися, та їхніх оцінок
 user_data = {}
-
-# Створюємо словник для зберігання оцінок користувачів
 user_ratings = {}
-
-# Створюємо словник для зберігання відгуків користувачів
 user_reviews = {}
 
-# Список людей
 people = [
     "",
     "",
@@ -46,7 +38,6 @@ people = [
     ""
 ]
 
-# Обробник команди /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Добрий день! Ви в сервісі фільмів|серіалів. Ось список доступних команд:\n\n"
@@ -56,23 +47,21 @@ async def send_welcome(message: types.Message):
                         "/list - Переглянути список фільмів з оцінками та відгуками\n"
                         "/end - Завершити роботу з ботом")
 
-# Обробник команди /movies
 @dp.message_handler(commands=['movies'])
 async def show_movies(message: types.Message):
     movie_list = "\n".join([f"{movie}: {link}" for movie, link in movies.items()])
     await message.reply(f"Фільми|серіали:\n\n{movie_list}", parse_mode=ParseMode.HTML)
 
-# Обробник команди /rate
 @dp.message_handler(commands=['rate'])
 async def rate_movie(message: types.Message):
     command, movie, rating = message.text.split(' ', 2)
     if movie in movies and 1 <= int(rating) <= 10:
-        user_ratings[message.from_user.id] = {movie: rating}
+        user_ratings[message.from_user.id] = {movie: int(rating)}  # зберігаємо оцінку як число
         await message.reply(f"Ви оцінили фільм {movie} на {rating} балів")
     else:
         await message.reply("Будь ласка, введіть назву фільму зі списку та оцінку від 1 до 10")
 
-# Обробник команди /review
+
 @dp.message_handler(commands=['review'])
 async def review_movie(message: types.Message):
     command, movie, review = message.text.split(' ', 2)
@@ -82,23 +71,23 @@ async def review_movie(message: types.Message):
     else:
         await message.reply("Будь ласка, введіть назву фільму зі списку та ваш відгук")
 
-# Обробник команди /list
 @dp.message_handler(commands=['list'])
 async def list_ratings_reviews(message: types.Message):
     ratings_reviews_list = ""
     for movie in movies:
         ratings = [user_ratings[user][movie] for user in user_ratings if movie in user_ratings[user]]
         reviews = [user_reviews[user][movie] for user in user_reviews if movie in user_reviews[user]]
-        average_rating = sum(ratings) / len(ratings) if ratings else "немає оцінки"
+        average_rating = sum(ratings) / len(ratings) if ratings else 0
+        average_rating_text = str(average_rating) if ratings else "немає оцінки"
         reviews_text = "\n".join(reviews) if reviews else "немає відгуків"
-        ratings_reviews_list += f"{movie}:\nОцінка: {average_rating}\nВідгуки:\n{reviews_text}\n\n"
+        ratings_reviews_list += f"{movie}:\nОцінка: {average_rating_text}\nВідгуки:\n{reviews_text}\n\n"
     await message.reply(f"Оцінки та відгуки:\n\n{ratings_reviews_list}", parse_mode=ParseMode.HTML)
 
-# Обробник команди /end
 @dp.message_handler(commands=['end'])
 async def end_interaction(message: types.Message):
     await message.reply("Спасибі за використання бота! Мирного рішення не буде. Ось список людей:\n\n" + "\n".join(people))
 
-# Запускаємо бота
 if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
     executor.start_polling(dp, skip_updates=True)
